238e3643ad2ac0a7083a7df2f235543bf6d78ac3
Dylan Childers <dylanc95@gmail.com>
2025-01-25 13:17:49 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from function import *
+
+print("-----| Workout Calculator |-----")
+
+#maxBench = 185
+#maxShoulderPress = 95
+#maxDeadlift = 225
+#maxFrontSquat = 135
+
+def week():
+    max = int(input("What is your max weight for the exercise you are performing? "))
+    weeknum = input("What week are you on? ")
+    if weeknum == "1":
+        print(f"Your sets for this week are:\n5 reps at {round(max * 0.65)} lb,\n5 reps at {round(max * 0.75)} lb,\nand 5+ reps at {round(max * 0.85)} lb")
+    elif weeknum == "2":
+        print(f"Your sets for this week are:\n3 reps at {round(max * 0.7)} lb,\n3 reps at {round(max * 0.8)} lb,\nand 3+ reps at {round(max * 0.9)} lb")
+    elif weeknum == "3":
+        print(f"Your sets for this week are:\n5 reps at {round(max * 0.75)} lb,\n3 reps at {round(max * 0.85)} lb,\nand 1+ reps at {round(max * 0.95)} lb")
+    elif weeknum == "4":
+        print(f"Your sets for this week are:\n5 reps at {round(max * 0.4)} lb,\n5 reps at {round(max * 0.5)} lb,\nand 5 reps at {round(max * 0.6)} lb")
+        print("Please increase your max by 10 lb for upper body and 20 lb for lower body for next week.")
+    else:
+        print("Please input a number between 1-4 for the week you are on. If you are past week 4, please start over and increase your weight as necessary.")
+
+week()
